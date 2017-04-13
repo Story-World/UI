6e80e6662e9e05e898d490295f50be56e1c4372b
Komputer\Grzegorz <gryman11@gmail.com>
2017-04-13 19:26:40 +0200

    User menu added

--- src/app/components/menu.component.ts
+++ src/app/components/menu.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { UserDataProvider } from '../services/userDataProvider.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'menu',
-  templateUrl: `../app/views/menu.html`,
-  styleUrls: [`../app/styles/styles.css`]
+	selector: 'menu',
+	templateUrl: `../app/views/menu.html`,
+	styleUrls: [`../app/styles/styles.css`]
 })
-export class MenuComponent  {}
+export class MenuComponent  {
+	constructor(private userDataProvider:UserDataProvider){
+		
+	}
+
+	public isLoggedIn(){
+		return this.userDataProvider.isLoggedIn();
+	}
+
+	public setLoggedIn(logged: boolean){
+		this.userDataProvider.setLoggedIn(logged);
+	}
+}
